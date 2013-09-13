ff2de55d97805ed04113ef42d7ab1c7a97c2ae16
Luka Gabric <luka.gabric@gmail.com>
2013-09-13 11:39:43 +0200

    aes256 and base64

--- LHelper/Classes/LHelperCategories.h
+++ LHelper/Classes/LHelperCategories.h
@@ -133,8 +133,6 @@
 - (BOOL)afterDate:(NSDate *)date;
 - (BOOL)afterOrEqualToDate:(NSDate *)date;
 - (NSString *)stringWithFormat:(NSString *)format;
-- (NSString *)stringWithFormat:(NSString *)format useGMT:(BOOL)useGMT;
-- (NSString *)stringWithFormat:(NSString *)format andTimeZone:(NSTimeZone *)timeZone;
 
 
 @end
@@ -160,7 +158,9 @@
 
 - (NSDate *)dateWithFormat:(NSString *)format;
 - (NSString *)md5;
-- (NSData *)base64StringToData;
+- (NSData *)dataFromBase64String;
+- (NSString *)AES256EncryptedBase64StringWithKey:(NSString *)key;
+- (NSString *)AES256DecryptedBase64StringWithKey:(NSString *)key;
 
 
 @end
@@ -171,9 +171,22 @@
 
 @interface NSData (NSData_LHelperCategories)
 
+void *NewBase64Decode(
+                      const char *inputBuffer,
+                      size_t length,
+                      size_t *outputLength);
+
+char *NewBase64Encode(
+                      const void *inputBuffer,
+                      size_t length,
+                      bool separateLines,
+                      size_t *outputLength);
 
 - (NSString *)stringUsingEncoding:(NSStringEncoding)encoding;
 - (NSString *)md5;
+- (NSData *)AES256EncryptWithKey:(NSString *)key;
+- (NSData *)AES256DecryptWithKey:(NSString *)key;
++ (NSData *)dataFromBase64String:(NSString *)aString;
 - (NSString *)base64EncodedString;
 
 
@@ -202,13 +215,6 @@ typedef void (^LCancelBlock)();
 
 
 @end
-
-
-#pragma mark - Base64
-
-
-void *LNewBase64Decode(const char *inputBuffer, size_t length, size_t *outputLength);
-char *LNewBase64Encode(const void *buffer, size_t length, bool separateLines, size_t *outputLength);
 
 
 #pragma mark -
